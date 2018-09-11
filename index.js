1f9cba7da7c9424f5d207aca9a3fbca4674b65e7
Jozsef Sipos <jozsef.sipos@betssongroup.com>
2018-09-11 14:25:08 +0200

    add fullname and players to change actions

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ window.onload = function (e) {
             },
             history: []
         },
+        computed: {
+            players: function() {
+                return [
+                    this.red.striker,
+                    this.red.defense,
+                    this.blue.striker,
+                    this.blue.defense
+                ].filter(p => p != null);
+            }
+        },
         methods: {
             score: function (player) {
                 var color = null;
@@ -40,17 +50,23 @@ window.onload = function (e) {
                     timestamp: moment().format("HH:mm:ss")
                 });
             },
-            setPlayer: function(team, position) {
+            setPlayer: function (team, position) {
                 if (team === "red" && position === "defense") {
-                    this.red.defense = { name: "Sanyi", dept: "Puli" };
+                    this.red.defense = { name: "Sanyi", dept: "Puli", fullName: "Sandor Koncz" };
                 } else if (team === "red" && position === "striker") {
-                    this.red.striker = { name: "Józsi", dept: "Puli" };
+                    this.red.striker = { name: "Józsi", dept: "Puli", fullName: "Jozsef Sipos" };
                 } else if (team === "blue" && position === "defense") {
-                    this.blue.defense = { name: "Marci", dept: "Comms" };
+                    this.blue.defense = { name: "Marci", dept: "Comms", fullName: "Marcell Miso" };
                 } else if (team === "blue" && position === "striker") {
-                    this.blue.striker = { name: "Rudi", dept: "Comms" };
+                    this.blue.striker = { name: "Rudi", dept: "Comms", fullName: "Rudolf Serfozo" };
                 } else {
                     console.error("Invalid team and/or position: [" + team + "], [" + position + "]");
+                }
+            },
+            removeHistoryItem: function (item) {
+                var index = this.history.indexOf(item);
+                if (index > -1) {
+                    this.history.splice(index, 1);
                 }
             }
         }
