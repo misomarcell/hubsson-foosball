190cb4f12d4cd441c94ab2a616c2996ee7f50187
Jozsef Sipos <jozsef.sipos@betssongroup.com>
2018-09-11 13:03:29 +0200

    add ability to set players and to score

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 $('.dropdown').dropdown({
     transition: 'drop'
 });
+
+window.onload = function (e) {
+    var app = new Vue({
+        el: "#app",
+        data: {
+            red: {
+                score: 0,
+                striker: null,
+                defense: null
+            },
+            blue: {
+                score: 0,
+                striker: null,
+                defense: null
+            }
+        },
+        methods: {
+            score: function(player) {
+                if (player === this.red.striker || player === this.red.defense) {
+                    this.red.score++;
+                } else if (player === this.blue.striker || player === this.blue.defense) {
+                    this.blue.score++;
+                } else {
+                    console.error("Cannot determine team for " + player);
+                }
+            },
+            setPlayer: function(team, position) {
+                if (team === "red" && position === "defense") {
+                    this.red.defense = { name: "Sanyi", dept: "Puli" };
+                } else if (team === "red" && position === "striker") {
+                    this.red.striker = { name: "Józsi", dept: "Puli" };
+                } else if (team === "blue" && position === "defense") {
+                    this.blue.defense = { name: "Marci", dept: "Comms" };
+                } else if (team === "blue" && position === "striker") {
+                    this.blue.striker = { name: "Rudi", dept: "Comms" };
+                } else {
+                    console.error("Invalid team and/or position: [" + team + "], [" + position + "]");
+                }
+            }
+        }
+    });
+}
